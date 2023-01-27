0e9242ef7c1b511b3693785272695d69ca3d6311
nmke-de <github@nmke.de>
2023-01-27 02:15:50 +0100

    Outsourced outcommented Stockfish

--- chess.go
+++ chess.go
@@ -8,33 +8,6 @@ import (
 	//"github.com/notnil/chess/uci"
 )
 
-/*type Stockfish bool
-func (this Stockfish) move(game *chess.Game) *chess.Move {
-	valid := game.ValidMoves()
-	eng, err := uci.New("stockfish")
-	if err != nil {
-		panic(err)
-	}
-	defer eng.Close()
-	// initialize uci with new game
-	if err := eng.Run(uci.CmdUCI, uci.CmdIsReady, uci.CmdUCINewGame); err != nil {
-		panic(err)
-	}
-	cmdPos := uci.CmdPosition{Position: game.Position()}
-	cmdGo := uci.CmdGo{MoveTime: time.Second / 100}
-	if err := eng.Run(cmdPos, cmdGo); err != nil {
-		return valid[rand.Intn(len(valid))]
-	}
-	move := eng.SearchResults().BestMove
-	if err := game.Move(move); err != nil {
-		return valid[rand.Intn(len(valid))]
-	}
-	return move
-}
-func (this Stockfish) name() string {
-	return "Stockfish"
-}*/
-
 type MaxOpponentMoves bool
 func (this MaxOpponentMoves) move(game *chess.Game) *chess.Move {
 	valid := game.ValidMoves()
