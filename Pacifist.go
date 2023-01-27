9544058c77d8427cd65a8b5eef2ee1945bf612f6
nmke-de <github@nmke.de>
2023-01-27 02:01:33 +0100

    Outsourced Pacifist

---
+++ Pacifist.go
@@ -0,0 +1,42 @@
+package main
+
+import "github.com/notnil/chess"
+
+type Pacifist bool
+func (this Pacifist) move(game *chess.Game) *chess.Move {
+	valid := game.ValidMoves()
+	var chosen *chess.Move
+	chosen = valid[0]
+	var ismate *chess.Game
+	ceval := 0
+	for _, m := range valid {
+		eval := 0
+		// Check for checkmate
+		ismate = game.Clone()
+		ismate.Move(m)
+		if ismate.Method() == 1 {
+			eval -= 110
+		// Check for check
+		} else if m.HasTag(chess.Check) {
+			eval -= 100
+		}
+		// Check for capture
+		if game.Position().Board().Piece(m.S2()).Type() != chess.NoPieceType {
+			switch game.Position().Board().Piece(m.S2()).Type() {
+				case chess.Pawn: eval -= 1
+				case chess.Bishop, chess.Knight: eval -= 3
+				case chess.Rook: eval -= 5
+				case chess.Queen: eval -= 9
+				default: eval -= 0
+			}
+		}
+		if eval > ceval {
+			ceval = eval
+			chosen = m
+		}
+	}
+	return chosen
+}
+func (this Pacifist) name() string {
+	return "Pacifist"
+}
