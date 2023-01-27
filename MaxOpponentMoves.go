6a35d5649838f645ac1fe975e1a164fd0bdfe33b
nmke-de <github@nmke.de>
2023-01-27 02:16:39 +0100

    Outsourced MaxOpponentMoves

---
+++ MaxOpponentMoves.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/notnil/chess"
+
+type MaxOpponentMoves bool
+func (this MaxOpponentMoves) move(game *chess.Game) *chess.Move {
+	valid := game.ValidMoves()
+	var chosen *chess.Move
+	var next *chess.Game
+	ceval := -1
+	for _, m := range valid {
+		next = game.Clone()
+		next.Move(m)
+		eval := len(next.ValidMoves())
+		if eval > ceval {
+			ceval = eval
+			chosen = m
+		}
+	}
+	return chosen
+}
+func (this MaxOpponentMoves) name() string {
+	return "MaxOpponentMoves"
+}
--- chess.go
+++ chess.go
@@ -8,27 +8,6 @@ import (
 	//"github.com/notnil/chess/uci"
 )
 
-type MaxOpponentMoves bool
-func (this MaxOpponentMoves) move(game *chess.Game) *chess.Move {
-	valid := game.ValidMoves()
-	var chosen *chess.Move
-	var next *chess.Game
-	ceval := -1
-	for _, m := range valid {
-		next = game.Clone()
-		next.Move(m)
-		eval := len(next.ValidMoves())
-		if eval > ceval {
-			ceval = eval
-			chosen = m
-		}
-	}
-	return chosen
-}
-func (this MaxOpponentMoves) name() string {
-	return "MaxOpponentMoves"
-}
-
 type SuicideKing bool
 func (this SuicideKing) move(game *chess.Game) *chess.Move {
 	valid := game.ValidMoves()
