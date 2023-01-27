01c1c7b94f5480b60fbba7a9ff8546b962ea65aa
nmke-de <github@nmke.de>
2023-01-27 01:58:22 +0100

    Outsourced MinOpponentMoves

---
+++ MinOpponentMoves.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/notnil/chess"
+
+type MinOpponentMoves bool
+func (this MinOpponentMoves) move(game *chess.Game) *chess.Move {
+	valid := game.ValidMoves()
+	var chosen *chess.Move
+	var next *chess.Game
+	ceval := 8192
+	for _, m := range valid {
+		next = game.Clone()
+		next.Move(m)
+		eval := len(next.ValidMoves())
+		if eval < ceval {
+			ceval = eval
+			chosen = m
+		}
+	}
+	return chosen
+}
+func (this MinOpponentMoves) name() string {
+	return "MinOpponentMoves"
+}
--- chess.go
+++ chess.go
@@ -8,27 +8,6 @@ import (
 	//"github.com/notnil/chess/uci"
 )
 
-type MinOpponentMoves bool
-func (this MinOpponentMoves) move(game *chess.Game) *chess.Move {
-	valid := game.ValidMoves()
-	var chosen *chess.Move
-	var next *chess.Game
-	ceval := 8192
-	for _, m := range valid {
-		next = game.Clone()
-		next.Move(m)
-		eval := len(next.ValidMoves())
-		if eval < ceval {
-			ceval = eval
-			chosen = m
-		}
-	}
-	return chosen
-}
-func (this MinOpponentMoves) name() string {
-	return "MinOpponentMoves"
-}
-
 type Pacifist bool
 func (this Pacifist) move(game *chess.Game) *chess.Move {
 	valid := game.ValidMoves()
