f049cbf47244485e0443f03465e84a078d487bcb
nmke-de <github@nmke.de>
2023-01-27 02:24:37 +0100

    Outsourced Huddle

--- chess.go
+++ chess.go
@@ -8,49 +8,6 @@ import (
 	//"github.com/notnil/chess/uci"
 )
 
-type Huddle bool
-func (this Huddle) move(game *chess.Game) *chess.Move {
-	valid := game.ValidMoves()
-	// Determine color and target king
-	color := game.Position().Board().Piece(valid[0].S1()).Color()
-	var target chess.Piece
-	if color == chess.White {
-		target = chess.WhiteKing
-	} else {
-		target = chess.BlackKing
-	}
-	// Determine position of target king
-	var sq chess.Square
-	for i := 0; i < 64; i++ {
-		sq = chess.Square(i)
-		if game.Position().Board().Piece(sq) == target {
-			break
-		}
-	}
-	// Evaluate all valid moves
-	var chosen *chess.Move
-	ceval := 85
-	for _, m := range valid {
-		// Only interested in not the king
-		if game.Position().Board().Piece(m.S1()).Type() == chess.King {
-			continue
-		}
-		eval := KingDist(m.S2(), sq)
-		if eval < ceval {
-			ceval = eval
-			chosen = m
-		}
-	}
-	// Choose randomly if no move was interesting
-	if chosen == nil {
-		return valid[rand.Intn(len(valid))]
-	}
-	return chosen
-}
-func (this Huddle) name() string {
-	return "Huddle"
-}
-
 type Swarm bool
 func (this Swarm) move(game *chess.Game) *chess.Move {
 	valid := game.ValidMoves()
