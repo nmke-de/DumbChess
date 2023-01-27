15a9bceafbe0d3daebe93da0543401924d57c704
nmke-de <github@nmke.de>
2023-01-27 17:25:12 +0100

    Bugfixed Schizophrenia

--- Schizophrenia.go
+++ Schizophrenia.go
@@ -8,11 +8,12 @@ func (this Schizophrenia) move(game *chess.Game) *chess.Move {
 	var p Player
 	h := Human(true)
 	s := Schizophrenia(true)
-	for p == nil && p.name() != h.name() && p.name() != s.name() {
+	for p == nil || p.name() == h.name() || p.name() == s.name() {
 		p = RandomPlayer()
 	}
 	return p.move(game)
 }
+
 func (this Schizophrenia) name() string {
 	return "Schizophrenia"
 }
