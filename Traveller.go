e2562779caa41b85e01b503635ee0392413ee265
nmke-de <github@nmke.de>
2023-01-28 13:57:19 +0100

    Improved Traveller

--- Traveller.go
+++ Traveller.go
@@ -7,16 +7,18 @@ type Traveller bool
 
 func (this Traveller) move(game *chess.Game) *chess.Move {
 	valid := game.ValidMoves()
-	var chosen *chess.Move
+	var best []*chess.Move
 	ceval := 0
 	for _, m := range valid {
 		eval := ManhattanDist(m.S1(), m.S2())
-		if eval >= ceval {
+		if eval > ceval {
 			ceval = eval
-			chosen = m
+			best = []*chess.Move{m}
+		} else if eval == ceval {
+			best = append(best, m)
 		}
 	}
-	return chosen
+	return TieBreak(best)
 }
 
 func (this Traveller) name() string {
